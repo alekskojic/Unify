0d1cc31c2f4256443da3ff221aa2eb30c6a27ecc
alekskojic <aleksandarkojic@Aleksandar-Kojics-MacBook.local>
2016-12-02 11:30:14 -0500

    change logo connected to mlab db

--- back_end/index.js
+++ back_end/index.js
@@ -8,24 +8,24 @@ const login_routes					 = require('./routes/login');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(express.static(__dirname + './../front_end/Unify/build'));
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 //connect to mongoose
-mongoose.connect('mongodb://localhost/data/db/');
+mongoose.connect('mongodb://alekskojic:Unify1@ds119728.mlab.com:19728/unifydatabase');
 
 //console.log a successful connection or if there's an error
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log("Connected to db at /data/db/")
+  console.log("Connected to db at unifydatabase")
 });
 
 app.use('/api/volunteers', volunteers_routes);
